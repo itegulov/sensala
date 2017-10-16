3dcebb0bbc8ce78eea115a70b2ea690bc9d30a7a
Daniyar Itegulov <ditegulov@gmail.com>
2017-10-16 12:44:58 +1100

    Implement simple sentence parser based on Stanford Parser

---
+++ src/main/scala/sensala/parser/SentenceParser.scala
@@ -0,0 +1,58 @@
+package sensala.parser
+
+import java.io.StringReader
+
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser
+import edu.stanford.nlp.process.{CoreLabelTokenFactory, PTBTokenizer}
+import edu.stanford.nlp.trees.Tree
+import sensala.structure._
+
+object SentenceParser {
+  private val parserModel = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz"
+  private val lp = LexicalizedParser.loadModel(parserModel)
+
+  def extractNounPhrase(tree: Tree): NounPhrase = {
+    tree.label.value match {
+      case "NP" =>
+        val nounWords = tree.children.map {
+          child => child.label.value match {
+            case "NN" => ProperNoun(child.getChild(0).label.value)
+            case "NNP" => CommonNoun(child.getChild(0).label.value)
+            case "PRP" => ReflexivePronoun(child.getChild(0).label.value)
+          }
+        }
+        nounWords.head
+    }
+  }
+
+  def extractVerbPhrase(tree: Tree): VerbPhrase = {
+    tree.label.value match {
+      case "VP" if tree.children.length == 1 =>
+        IntransitiveVerb(tree.getChild(0).getChild(0).label.value)
+      case "VP" if tree.children.length == 2 =>
+        val transitiveVerb = TransitiveVerb(tree.getChild(0).getChild(0).label.value)
+        VerbObjPhrase(transitiveVerb, extractNounPhrase(tree.getChild(1)))
+    }
+  }
+
+  def convert(tree: Tree): Sentence = {
+    tree.pennPrint()
+    val s = tree.getChild(0)
+    val nounPhrase = s.children.find(_.label.value == "NP")
+    val verbPhrase = s.children.find(_.label.value == "VP")
+    (nounPhrase, verbPhrase) match {
+      case (Some(noun), Some(verb)) =>
+        Sentence(extractNounPhrase(noun), extractVerbPhrase(verb))
+      case _ =>
+        sys.error("Invalid sentence")
+    }
+  }
+
+  def parse(text: String): Sentence = {
+    val tokenizerFactory = PTBTokenizer.factory(new CoreLabelTokenFactory(), "")
+    val tok = tokenizerFactory.getTokenizer(new StringReader(text))
+    val words = tok.tokenize()
+    val tree = lp.apply(words)
+    convert(tree)
+  }
+}
