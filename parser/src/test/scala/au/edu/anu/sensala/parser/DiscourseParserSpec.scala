6bbbd64a31c747e7d59662411a3fac02181c1533
Daniyar Itegulov <ditegulov@gmail.com>
2017-10-23 15:48:57 +1100

    Adapt tests for discourse parsing and add new multi-sentence tests

--- parser/src/test/scala/au/edu/anu/sensala/parser/DiscourseParserSpec.scala
+++ parser/src/test/scala/au/edu/anu/sensala/parser/DiscourseParserSpec.scala
@@ -5,13 +5,49 @@ import au.edu.anu.sensala.structure._
 
 class DiscourseParserSpec extends SensalaSpec {
   it should "parse simple sentences" in {
-    DiscourseParser.parse("John walks") shouldBe Sentence(ProperNoun("John"), IntransitiveVerb("walks"))
-    DiscourseParser.parse("Mary loves herself") shouldBe Sentence(ProperNoun("Mary"), VerbObjPhrase(TransitiveVerb("loves"), ReflexivePronoun("herself")))
+    DiscourseParser.parse("John walks") shouldBe Discourse(List(
+      Sentence(ProperNoun("John"), IntransitiveVerb("walks"))
+    ))
+    DiscourseParser.parse("Mary loves herself") shouldBe Discourse(List(
+      Sentence(ProperNoun("Mary"), VerbObjPhrase(TransitiveVerb("loves"), ReflexivePronoun("herself")))
+    ))
   }
 
   it should "parse quantified common nouns" in {
-    DiscourseParser.parse("A donkey walks") shouldBe Sentence(ExistentialQuantifier(CommonNoun("donkey")), IntransitiveVerb("walks"))
-    DiscourseParser.parse("Every farmer walks") shouldBe Sentence(ForallQuantifier(CommonNoun("farmer")), IntransitiveVerb("walks"))
-    DiscourseParser.parse("Every farmer owns a donkey") shouldBe Sentence(ForallQuantifier(CommonNoun("farmer")), VerbObjPhrase(TransitiveVerb("owns"), ExistentialQuantifier(CommonNoun("donkey"))))
+    DiscourseParser.parse("A donkey walks") shouldBe Discourse(List(
+      Sentence(ExistentialQuantifier(CommonNoun("donkey")), IntransitiveVerb("walks"))
+    ))
+    DiscourseParser.parse("Every farmer walks") shouldBe Discourse(List(
+      Sentence(ForallQuantifier(CommonNoun("farmer")), IntransitiveVerb("walks"))
+    ))
+    DiscourseParser.parse("Every farmer owns a donkey") shouldBe Discourse(List(
+      Sentence(ForallQuantifier(CommonNoun("farmer")), VerbObjPhrase(TransitiveVerb("owns"), ExistentialQuantifier(CommonNoun("donkey"))))
+    ))
+  }
+
+  it should "parse wh noun phrases" in {
+    DiscourseParser.parse("Every farmer who owns a donkey beats it") shouldBe Discourse(List(
+      Sentence(
+        ForallQuantifier(WhNounPhrase(VerbObjPhrase(TransitiveVerb("owns"), ExistentialQuantifier(CommonNoun("donkey"))), CommonNoun("farmer"))),
+        VerbObjPhrase(TransitiveVerb("beats"), ReflexivePronoun("it"))
+      )
+    ))
+  }
+
+  it should "parse multi-sentence discourses" in {
+    DiscourseParser.parse("Every farmer who owns a donkey beats it. John is a farmer. John owns a donkey.") shouldBe Discourse(List(
+      Sentence(
+        ForallQuantifier(WhNounPhrase(VerbObjPhrase(TransitiveVerb("owns"), ExistentialQuantifier(CommonNoun("donkey"))), CommonNoun("farmer"))),
+        VerbObjPhrase(TransitiveVerb("beats"), ReflexivePronoun("it"))
+      ),
+      Sentence(
+        ProperNoun("John"),
+        VerbObjPhrase(TransitiveVerb("is"), ExistentialQuantifier(CommonNoun("farmer")))
+      ),
+      Sentence(
+        ProperNoun("John"),
+        VerbObjPhrase(TransitiveVerb("owns"), ExistentialQuantifier(CommonNoun("donkey")))
+      )
+    ))
   }
 }
